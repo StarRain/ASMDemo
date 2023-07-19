ac3f9399df5fb6ccf5216f4f6871215039681e88
chuangjie.pcj <panchuangjie@163.com>
2023-07-19 16:03:13 +0800

    fix project

--- app/src/main/java/com/rainstar/asm/TestASMActivity.java
+++ app/src/main/java/com/rainstar/asm/TestASMActivity.java
@@ -1,10 +1,10 @@
 package com.rainstar.asm;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class TestASMActivity extends AppCompatActivity {
+public class TestASMActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
